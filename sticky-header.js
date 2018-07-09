50c7df4e307a2aae7eb7b263f24c46ea0739c4db
Javier Candalaft <jcandalaft@elementum.com>
2018-07-09 16:03:21 -0300

    Fixed content and fixed header jump

--- sticky-header.js
+++ sticky-header.js
@@ -4,8 +4,10 @@ window.onscroll = function() {
   console.log('window.scrollY', window.scrollY);
   if (window.scrollY >= 190) {
     document.querySelector('.navigation-menu').classList.add('fixed-header');
+    document.querySelector('.banner').classList.add('fixed-header');
   }
   else {
     document.querySelector('.navigation-menu').classList.remove('fixed-header');
+    document.querySelector('.banner').classList.remove('fixed-header');
   }
 };

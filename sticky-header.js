fc07ad9229bc048028b6defbcce7268add7cd54f
Javier Candalaft <jcandalaft@elementum.com>
2018-07-10 00:30:11 -0300

    removed console log

--- sticky-header.js
+++ sticky-header.js
@@ -1,5 +1,4 @@
 window.onscroll = function() {
-  console.log('window.scrollY', window.scrollY);
   if (window.scrollY >= 190) {
     document.querySelector('.navigation-menu').classList.add('fixed-header');
     document.querySelector('.banner').classList.add('fixed-header');
